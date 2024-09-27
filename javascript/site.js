3cc9348117895ef1580a02ab8b2110d185447286
Emilien Aboula <300095450@fvtc.edu>
2024-09-27 12:44:33 -0700

    Added secret message

--- javascript/site.js
+++ javascript/site.js
@@ -4,6 +4,7 @@ alert('JavaScript is successfully linked to this page.')
 // Change the color of my name when I hover over the section
 const section = document.querySelector('section')
 const animeName = section.querySelector('b')
+
 section.addEventListener('mouseover', () => {
     setTimeout(()=>{
     animeName.style.color = '#800517'
@@ -17,7 +18,16 @@ const hours = new Date().getHours() // get the current hour
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
+const key =" It's a secret to everybody"
 
+const message = `The key is the first argument accepted by method setItem().  
+It is a required string argument representing the name of 
+the web storage property(localStorage) you want to create or update.
+
+It also serves as the identifier for the data you are storing. 
+This key is used to retrieve the corresponding value later.` //My secret message:  key is a string that 
+
+//The welcome variable will vary, therefor we have to declare it with let              
 let welcome = document.querySelector('.welcome')
 
 if(isMorning){
@@ -27,3 +37,5 @@ if(isMorning){
 }else{
     welcome.textContent = 'Good Evening and welcome to my site!'
 }
+
+localStorage.setItem(key, message)
