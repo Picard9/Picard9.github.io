c6264331b933415730d12c589cf9d3473f0afe61
Emilien Aboula <300095450@fvtc.edu>
2024-10-10 00:04:36 -0700

    Assigment Week 5 Update

--- javascript/site.js
+++ javascript/site.js
@@ -18,7 +18,7 @@ const hours = new Date().getHours() // get the current hour
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
-const key =" It's a secret to everybody"
+const key ="It's a secret to everybody"
 
 const message = `The key is the first argument accepted by method setItem().  
 It is a required string argument representing the name of 
@@ -40,3 +40,50 @@ if(isMorning){
 
 // Store a message in local storage
 localStorage.setItem(key, message)
+
+
+// CODE FROM WEEK 5
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+ showImages() 
+
+const previous = document.querySelector('#prev')
+const next = document.querySelector('#next')
+const carousel = document.querySelector('#carousel')
+next.addEventListener('click', () => {
+
+
+    currentImage = (currentImage < urls.length - 1) ? currentImage +1 : currentImage;
+    showImages();
+});
+
+previous.addEventListener('click', () => {
+    currentImage = (currentImage > 0) ? currentImage -1 :currentImage; 
+    showImages();
+});
+
+ setInterval(() => {
+     // code to run EVERY 5 seconds
+     currentImage = (currentImage < urls.length - 1) ? currentImage + 1 : 0; // Loop back to 0 after the last image
+    showImages();
+ }, 5000)
+
+
